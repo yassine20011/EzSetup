bcc2f7351b80d2b471b25c9a0cfd0901fa110810
yassine20011 <medalgamer47@gmaim.com>
2025-02-18 19:51:07 +0100

    Add root command for EzSetup CLI tool

---
+++ cmd/root.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "Ezsetup",
+	Short: "CLI Tool for Quick Development Environment Setup",
+	Long: `EzSetup is a lightweight CLI tool that simplifies setting up development environments for beginners and mid-level IT students. 
+It automates installing essential tools, configuring environments, and setting up project templates for popular programming languages.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
+		} else {
+			cmd.Help()
+		}
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ezsetup.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
